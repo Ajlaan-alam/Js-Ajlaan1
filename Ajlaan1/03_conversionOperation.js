a427706b8c828ff1b7dcd9c432ea3ece1fc3f113
Ajlaan alam <ajlaan.alam1611@gmail.com>
2026-07-19 17:31:34 +0000

    conversion of datatypes

---
+++ Ajlaan1/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = false
+//onsole.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// "Ajlaan" => NaN
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.logeanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ajlaan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
+
